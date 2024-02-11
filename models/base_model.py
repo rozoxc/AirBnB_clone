9f23166a3fee3949b533617f3e91523e9d4dcb40
he <abdel.brave@gmail.com>
2024-02-11 10:54:52 -0800

    ld

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
 import uuid
-from datetime  import datetime
+from datetime import datetime
+'''write a class '''
+
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                        setattr(self, key, datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"))
                     else:
                         setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
-        self.created_at = datetime.now() 
- 
+        self.created_at = datetime.now()
+
     def to_dict(self):
         obj_dict = self.__dict__.copy()
         obj_dict['class'] = type(self).__name__
